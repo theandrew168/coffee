1862afde829e5e180f3f14c4864f796ae9761fab
brueland <wesbrueland@gmailcom>
2021-07-22 12:47:10 -0500

    add redirect to remove form resubmit, and remove https

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 import waitress
 from random import choice
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 
 app = Flask(__name__, root_path='.')
 
+
 @app.route('/', methods = ['GET', 'POST'])
-def index(current_buyer='None'):
+def index():
+    current_buyer = None
     potential_buyers = ['Derz', 'Wes']
     if request.method == 'POST':
         if 'random' in request.form.keys():
@@ -16,9 +18,11 @@ def index(current_buyer='None'):
             current_buyer = request.form['potential_buyers']
         with open('buyer.txt', 'w') as f:
             f.write(current_buyer)
+        return redirect('/')
     else:
-        with open('buyer.txt', 'r') as f:
-            current_buyer = f.read()
+        if os.path.isfile('buyer.txt'):
+            with open('buyer.txt', 'r') as f:
+                current_buyer = f.read()
 
     return render_template('index.html', current_buyer=current_buyer, potential_buyers=potential_buyers) 
 
@@ -27,4 +31,4 @@ if __name__ == '__main__':
     port = os.getenv('PORT') or '5000'
     addr = '127.0.0.1:{}'.format(port)
     print('Listening on {}'.format(addr))
-    waitress.serve(app, listen=addr, url_scheme='https')
+    waitress.serve(app, listen=addr)
